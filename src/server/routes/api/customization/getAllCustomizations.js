8e8b71be0f5fc276855257f4972584c43dc1c972
dtavana <datavana@gmail.com>
2022-10-03 01:16:29 -0400

    Implement logic for GET:/api/customization

--- src/server/routes/api/customization/getAllCustomizations.js
+++ src/server/routes/api/customization/getAllCustomizations.js
@@ -1,1 +1,6 @@
-module.exports = (req, res, next) => {};
+const Resource = require("../../../models/Resource");
+
+module.exports = async (req, res, next) => {
+  const resources = await Resource.find();
+  res.send(resources);
+};
