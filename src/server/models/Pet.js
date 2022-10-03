e69a50298b0cc6becc66a994d448ef3a2be0112e
dtavana <datavana@gmail.com>
2022-10-03 00:38:16 -0400

    Create mongoose models

---
+++ src/server/models/Pet.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Pet = new mongoose.Schema({
+  name: String,
+  hat: Number,
+  color: Number,
+  species: Number,
+  level: Number,
+  xp: Number,
+});
+const model = mongoose.model("Pet", Pet);
+
+module.exports = model;
