5813f88423baf79ef20c550cb5593c6420073778
dtavana <datavana@gmail.com>
2022-10-03 01:16:47 -0400

    Implement logic for GET:/api/resource/:resourceType/:resourceID

--- src/server/routes/api/resource/getResource.js
+++ src/server/routes/api/resource/getResource.js
@@ -1,1 +1,13 @@
-module.exports = (req, res, next) => {};
+const Resource = require("../../../models/Resource");
+
+module.exports = async (req, res, next) => {
+  const { resourceType, resourceID } = req.params;
+  const resource = await Resource.findOne({ resourceType, resourceID });
+  if (resource == null) {
+    res.status(404).send({
+      msg: `Could not find the resource ${resourceType}:${resourceID}`,
+    });
+  } else {
+    res.send(resource);
+  }
+};
