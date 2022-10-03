328a4faff89c6d3dc1f59c66a0dca97403b639d6
Samara Holmes <46973232+holmes1000@users.noreply.github.com>
2022-10-03 19:40:34 -0400

    added footer and wpi logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import Profile from './pages/profile.js';
@@ -13,6 +14,7 @@ function App() {
 return (
 	<Router>
 	<Navbar />
+	<Footer />
 	<Routes>
 		<Route path='/index' exact element={<Home />} />
 		<Route path='/profile' element={<Profile />} />
