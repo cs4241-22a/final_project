58f569913509a67b0620d0e529543f8cf28f38d7
Samara Holmes <46973232+holmes1000@users.noreply.github.com>
2022-10-04 14:24:23 -0400

    added in Login form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer';
@@ -9,20 +10,21 @@ import Listings from './pages/listings';
 import Shop from './pages/shop.js';
 import Messages from './pages/messages';
 import Login from './pages/login';
+import Auth from "./components/Login/Login"
 
 function App() {
 return (
 	<Router>
-	<Navbar />
-	<Footer />
-	<Routes>
-		<Route path='/index' exact element={<Home />} />
-		<Route path='/profile' element={<Profile />} />
-		<Route path='/messages' element={<Messages />} />
-		<Route path='/shop' element={<Shop />} />
-		<Route path='/listings' element={<Listings />} />
-		<Route path='/login' element={<Login />} />
-	</Routes>
+	  <Navbar />
+	  <Footer />
+	  <Routes>
+		  <Route path='/index' exact element={<Home />} />
+		  <Route path='/profile' element={<Profile />} />
+		  <Route path='/messages' element={<Messages />} />
+		  <Route path='/shop' element={<Shop />} />
+		  <Route path='/listings' element={<Listings />} />
+		  <Route path='/login' element={<Auth />} />
+	  </Routes>
 	</Router>
 );
 }
