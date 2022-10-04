1940a11695ee3dc54077e942ce4c448eb3c3e34a
BuildTools <unconfigured@null.spigotmc.org>
2022-10-04 02:30:42 +0000

    WORKING CHESS BOARD!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
+import { parseFen } from "chessops/fen";
+import { Chess } from "chessops/chess";
+import {makeFen} from 'chessops/fen'
+
 
 export default function App(props) {
-  return (
-    <>
-      <Board
-        position={"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"}
-      />
-    </>
+
+
+  const [game, setGame] = useState(
+    Chess.fromSetup(
+      parseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap()
+    ).unwrap()
   );
+
+  return (
+    <Board
+      game={game}
+    />
+  );
+
 }
+
+
+/*
+
+*/
