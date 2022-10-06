6d9e64053c795de4f815da66e8801a01e78df81d
tjcoppola234 <37402171+tjcoppola234@users.noreply.github.com>
2022-10-06 16:49:35 -0400

    made connection to db

--- server.js
+++ server.js
@@ -8,4 +8,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.use(express.json());
 app.use(express.static('build'));
 
+let db = null;
+
+client.connect()
+  .then( () => {
+    // will only create collection if it doesn't exist
+    return client.db('musicSuggestion')
+  })
+  .then( _db => {
+    // store reference to collection
+    db = _db;
+  });
+
+
 app.listen(8080);
