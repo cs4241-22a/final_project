65bb65d63b7e8ae4024147b95ffb05ab7d4fa661
AidanMulcahey <ammulcahey@wpi.edu>
2022-10-06 17:44:37 -0400

    Home Page, Login, Sign up

--- server.js
+++ server.js
@@ -10,8 +10,20 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '/public')))
 
 
-app.get('/gamerTime', (req, res) => {
-    res.render('index')
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/createaccount', (req, res) => {
+    res.render('signup')
 })
 
 
