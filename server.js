c0ff24635abd736f167167a7c2f22f3cf4fa4975
Livingwell088 <58043224+Livingwell088@users.noreply.github.com>
2022-10-06 17:40:19 -0400

    Authenicationfajssajdsnajdsnajd, cant spell it apparently

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require("express"),
+    cookie = require("cookie-session"),
     app = express()
+
 
 app.use(express.static("public"));
 app.use(express.static("views"));
 
 app.use(express.json());
 
+app.use( cookie({
+    name: 'session',
+    keys: ['testkey1', 'testkey2']
+}))
+
+
 const clientId = '903729e701c50a0a540c';
 const clientSecret = '11c4d32e3febd2e660acae7b806438ff3c44384f';
 
-app.get('/github', (req, res) => {
-    // res.redirect("main.html");
-    res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}`);
-});
-
 const axios = require('axios');
 let token = null;
+
+app.get('/github', (req, res) => {
+    // res.redirect("main.html");
+    console.log(token)
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}`);
+});
 
 app.get('/oauth-callback', (req, res) => {
     const body = {
@@ -24,20 +33,34 @@ app.get('/oauth-callback', (req, res) => {
         code: req.query.code
     };
     console.log(body);
-    const opts = { headers: { accept: 'application/json' } };
-    axios.post(`https://github.com/login/oauth/access_token`, body, opts).
+    const header = { headers: { accept: 'application/json' } };
+    axios.post(`https://github.com/login/oauth/access_token`, body, header).
     then(res => res.data['access_token']).
     then(_token => {
         console.log('My token:', _token);
         token = _token;
         client.db( 'Final' ).collection( 'Users' ).insertOne({token: token});
-        res.redirect("/main.html");
+        // res.redirect("/main.html");
+        res.redirect("/loggedIn");
     }).
     catch(err => res.status(500).json({ message: err.message }));
 });
 
+app.get('/loggedIn', (req, res) => {
+    const header = { headers: { Authorization: "token " + token } };
+    axios.get("https://api.github.com/user", header)
+        .then(response => {
+
+            req.session.login = true;
+            req.session.user = response.data.login;
+            console.log(req.session.user);
+            res.redirect("/main.html");
+        })
+})
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const {response} = require("express");
 const uri = "mongodb+srv://admin:admin@final.nhlyk4z.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
