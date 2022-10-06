7b6b23ef348d9f6ee5b95a831eea08dc883a1984
asaduakas <arman.sdkas@gmail.com>
2022-10-05 22:59:38 -0400

    successfully running node+react+mongodb

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 
-import React from "react";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import Profile from './pages/profile.js';
+import Listings from './pages/listings';
+import Shop from './pages/shop.js';
+import Messages from './pages/messages';
+import Signin from "./components/Login/Login"
+import Create from "./components/Login/Create"
+import Goal from "./pages/goal"
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-        <button type="button" class="btn btn-primary">
-          This is a bootstrap button
-        </button>
-      </>
-    );
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Footer />
+      <Routes>
+        <Route path='/index' exact element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/messages' element={<Messages />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/listings' element={<Listings />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/create-an-account' element={<Create />} />
+        <Route path='/goal' element={<Goal />} />
+      </Routes>
+    </Router>
+  );
   }
-}
 
 export default App;
---
+++ src/pages/shop.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+  
+const Shop = () => {
+  return (
+    <div
+      style={{
+        display: 'block',
+        justifyContent: 'Left',
+        alignItems: 'Left',
+        height: '100vh',
+        padding: '2%'
+      }}
+    >
+      <h1>The GoatAShop</h1>
+    </div>
+  );
+};
+  
+export default Shop;
