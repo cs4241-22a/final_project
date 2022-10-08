e35c2a27f31cda728d1f6e372e5b504b6ab33c83
chjm6 <chjm655@gmail.com>
2022-10-08 12:28:36 -0400

    Starter static server

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+const port = '3000';
+// Setup static express
+const app = express();
+app.use(express.static('build'));
+const listenPort = process.env.PORT || port;
+app.listen(listenPort);
+console.log(`Listening on port ${listenPort}`);
