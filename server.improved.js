f325735e6a96fc83ef9f34ba5511ffb7a577d80f
Andrei Ignatiev <alignatiev@wpi.edu>
2022-10-09 19:56:44 -0400

    some updates

--- server.improved.js
+++ server.improved.js
@@ -48,7 +48,7 @@ app.use( express.urlencoded({ extended:true }) )
 // activate cookie handeling: session
 app.use( cookie({
   name: 'session',
-  keys: [process.env.SESSION_k1, process.env.SESSION_k2]
+  keys: [process.env.SESSION_K1, process.env.SESSION_K2]
 }))
 // serve all static pages as we need CSS. etc
 app.use(serveStatic(dir, { index: ['login.html', 'login.htm'] }));
