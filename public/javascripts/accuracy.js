2b426f15ca7ccae0e94eae8618a8be012fb49340
jrkim-wpi <jkim6@wpi.edu>
2022-10-09 17:38:59 -0400

    feat: random hidden btn in acc

---
+++ public/javascripts/accuracy.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+
+    const randBtn = document.getElementsByClassName('randBtn');
+    const btnCont = document.getElementById('btnContainer');
+    const winWidth = btnCont.offsetWidth
+    const winHeight = btnCont.offsetHeight;
+
+    function getRandomNumber(min, max) {
+        return Math.random() * (max - min - 120) + min;
+    }
+
+    for( let i = 0; i < randBtn.length; i++) {
+        let currBtn = randBtn[i];
+        const randomTop = getRandomNumber(0, winHeight);
+        const randomLeft = getRandomNumber(0, winWidth);
+
+        currBtn.style.top = randomTop +"px";
+        currBtn.style.left = randomLeft +"px";
+    }
+
+    const statusText = document.getElementById("statusText")
+    const reactionBtn1 = document.getElementById("reactionBtn1")
+    const reactionBtn2 = document.getElementById("reactionBtn2")
+    const reactionBtn3 = document.getElementById("reactionBtn3");
+    const reactionBtn4 = document.getElementById("reactionBtn4");
+    const reactionBtn5 = document.getElementById("reactionBtn5");
+}
