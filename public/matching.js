bbae0483e5c75fa4dc8c53d47e1386cfd35cb736
Livingwell088 <58043224+Livingwell088@users.noreply.github.com>
2022-10-10 12:08:00 -0400

    Matching page

--- public/matching.js
+++ public/matching.js
@@ -2,9 +2,28 @@
 
 window.onload = function() {
 
+    let user = null;
+
+    fetch('/getUser', {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    })
+        .then(response => (response.text()))
+        .then(text => {
+            user = text;
+            console.log(user);
+        })
+
     fetch('/getAllMatches', {
         method: 'GET',
         headers: {'Content-Type': 'application/json'}
     })
-        .then()
+        .then(response => (response.json()))
+        .then((json) => {
+            json.forEach((item) => {
+                if(item.user != user){
+                    console.log(item);
+                }
+            })
+        })
 }
