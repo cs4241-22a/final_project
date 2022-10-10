547c5fc4fadb987340a33d130dd8643c0761f3cb
Travis <travisthomps0n@hotmail.com>
2022-10-10 13:07:53 -0400

    added moved scriptjs code

--- src/main/app/App.js
+++ src/main/app/App.js
@@ -1,5 +1,4 @@
 import MongoManager from "../../util/MongoManager";
-import Climb from "./route/Climb";
 import GymMap from "./visuals/GymMap";
 
 let instance = null
@@ -18,6 +17,75 @@ export default class App
         this.mongoManger = new MongoManager()
 
         this.gymMap = new GymMap()
-        this.routes = this.mongoManger.getAllRoutes();
+
+        //Load all of the routes in the database
+        this.mongoManger.getAllRoutes().then(routes => {
+            this.routes = routes
+            //update grade list
+            //update climb types
+            //List of filtered climbs
+
+           // this.populateFilters()
+        });
     }
+
+    
+    populateFilters() {
+        climbsArray.forEach(climb => addOption(climb))
+        
+        const gradesFilter = document.getElementById('grades');
+        
+        const climbTypeFilter = document.getElementById('climbType');
+        let climbs = [...new Set(route.map(climb => climb.type))]
+        climbs.forEach(climb => {
+            climbTypeFilter.options[climbTypeFilter.options.length] = new Option(climb.type, climb.type)
+
+        })
+
+    }
+    
+    populateGradeFilters()
+    {
+
+    }
+
+    //Add the option to the select
+    addOption(climb) {
+        if (!optionExists(climb.grade, gradesFilter)){
+            gradesFilter.options[gradesFilter.options.length] = new Option(climb.grade, climb.grade)
+        }
+    }
+    
+    optionExists ( needle, haystack )
+    {
+        let optionExists = false,
+            optionsLength = haystack.length;
+    
+        while ( optionsLength-- )
+        {
+            if ( haystack.options[ optionsLength ].value === needle )
+            {
+                optionExists = true;
+                break;
+            }
+        }
+        return optionExists;
+    }
+    
+    sortSelect(selElem) {
+        let tmpAry = [];
+        for (let i=0;i<selElem.options.length;i++) {
+            tmpAry[i] = [];
+            tmpAry[i][0] = selElem.options[i].text;
+            tmpAry[i][1] = selElem.options[i].value;
+        }
+        tmpAry.sort();
+        while (selElem.options.length > 0) {
+            selElem.options[0] = null;
+        }
+        for (let i=0;i<tmpAry.length;i++) {
+            selElem.options[i] = new Option(tmpAry[i][0], tmpAry[i][1]);
+        }
+    }
+    
 }
