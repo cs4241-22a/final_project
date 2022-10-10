55a808d808296a0f8d55f5a8d3967f792f20d151
Joe Dobbelaar <joedobbelaar@gmail.com>
2022-10-10 11:33:56 -0400

    POST /new-user and POST /existing-user

--- server.js
+++ server.js
@@ -234,6 +234,100 @@ app.post("/remove-item", (req, resp) => {
     })
 })
 
+app.post("/new-user", (req, resp) => {
+    const data = req.body;
+    client.connect((err, client) => {
+        if (err) {
+            throw err;
+        } else {
+            // Connection succeeded
+            const db = client.db("database");
+            db.collection("users").findOne({"email": data.email}, {}, (err, res) => {
+                if (err) {
+                    throw err;
+                } else {
+                    if (res) {
+                        const body = {
+                            error: true
+                        }
+                        console.log("User already exists.");
+                        resp.json(JSON.stringify(body));
+                        resp.end();
+                    } else {
+                        bcrypt.hash(data.password, saltRounds, function(err, hash) {
+                            // Hash pass
+                            const newUser = {
+                                email: data.email,
+                                password: hash,
+                                saveData: data.progressCheck,
+                                history: []
+                            }
+                            db.collection("users").insertOne(newUser, (err, res) => {
+                                // Insert to DB
+                                if (err) {
+                                    throw err;
+                                } else {
+                                    console.log("New user sent to database!");
+                                    const body = {
+                                        error: false,
+                                        email: data.email,
+                                    }
+                                    resp.json(JSON.stringify(body));
+                                    resp.end();
+                                }
+                            })
+                        });
+                    }
+                }
+            })
+        }
+    })
+})
+
+app.post("/existing-user", (req, resp) => {
+    const data = req.body;
+    client.connect((err, client) => {
+        if (err) {
+            throw err;
+        } else {
+            // Connection successful
+            const db = client.db("database");
+            db.collection("users").findOne({"email": data.email}, {}, (err, res) => {
+                if (err) {
+                    throw err;
+                } else {
+                    if (!res) {
+                        const body = {
+                            error: true
+                        }
+                        resp.json(JSON.stringify(body));
+                        resp.end();
+                    } else {
+                        const hash = res.password;
+                        bcrypt.compare(data.password, hash, (err, bcryptres) => {
+                            // Check if password is right
+                            if (bcryptres) {
+                                const body = {
+                                    error: false,
+                                    email: data.email,
+                                }
+                                resp.json(JSON.stringify(body));
+                                resp.end();
+                            } else {
+                                const body = {
+                                    error: true
+                                }
+                                resp.json(JSON.stringify(body));
+                                resp.end();
+                            }
+                        })
+                    }
+                }
+            })
+        }
+    })
+})
+
 // Default get
 app.get("*", (req, res) => {
     res.sendFile(__dirname + "/build/index.html")
