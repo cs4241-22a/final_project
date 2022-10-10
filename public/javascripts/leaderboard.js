4df7400765592ff5a1950fe61052e34cb023cf8a
seansta18 <sdmcmillan@wpi.edu>
2022-10-10 16:54:24 -0400

    Added button functionality with changing tables

---
+++ public/javascripts/leaderboard.js
@@ -0,0 +1,57 @@
+window.onload = function() {
+    const cpsBtn = document.getElementById("cpsBoardBtn");
+    const reactionBtn = document.getElementById("reactionBoardBtn");
+    const accuracyBtn = document.getElementById("accuracyBoardBtn");
+    const table = document.getElementById("leaderboard-table");
+
+    cpsBtn.addEventListener('click', function () {
+        //GET Table to update
+        while(table.rows.length !== 0)
+        {
+            table.deleteRow(0);
+        }
+        
+        var row = table.insertRow(0);
+        var userCell = row.insertCell(0);
+        var scoreCell = row.insertCell(1);
+        
+        userCell.innerHTML = "Username";
+        userCell.style = "padding-left: 30px;padding-right: 40px;font-weight: bold;";
+        scoreCell.innerHTML = "Clicks Per Second";
+        scoreCell.style = "padding-left: 30px;padding-right: 40px;font-weight: bold;";
+    })
+
+    reactionBtn.addEventListener('click', function () {
+        //GET Table to update
+        while(table.rows.length !== 0)
+        {
+            table.deleteRow(0);
+        }
+        
+        var row = table.insertRow(0);
+        var userCell = row.insertCell(0);
+        var scoreCell = row.insertCell(1);
+        
+        userCell.innerHTML = "Username";
+        userCell.style = "padding-left: 30px;padding-right: 40px;font-weight: bold;";
+        scoreCell.innerHTML = "Average Time";
+        scoreCell.style = "padding-left: 30px;padding-right: 40px;font-weight: bold;";
+    })
+
+    accuracyBtn.addEventListener('click', function () {
+        //GET Table to update
+        while(table.rows.length !== 0)
+        {
+            table.deleteRow(0);
+        }
+        
+        var row = table.insertRow(0);
+        var userCell = row.insertCell(0);
+        var scoreCell = row.insertCell(1);
+        
+        userCell.innerHTML = "Username";
+        userCell.style = "padding-left: 30px;padding-right: 40px;font-weight: bold;";
+        scoreCell.innerHTML = "Time";
+        scoreCell.style = "padding-left: 30px;padding-right: 40px;font-weight: bold;";
+    })
+}
