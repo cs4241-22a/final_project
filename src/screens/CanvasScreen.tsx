220652ae0815a7947942241a78e4627199508ad5
unknown <quinguyen.dev@gmail.com>
2022-10-10 21:48:36 -0400

    [NEW] Placeable color grid.

--- src/screens/CanvasScreen.tsx
+++ src/screens/CanvasScreen.tsx
@@ -1,59 +1,12 @@
-import { useEffect } from "react";
-import { ActiveEmoji, Header, Sidebar } from "../components";
-import { Box, Toolbar } from "@mui/material";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-
-function drawCanvas() {
-  const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
-
-  if (canvas !== null) {
-    const ctx = canvas.getContext("2d");
-
-    if (ctx !== null) {
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-    }
-  }
-}
+import { Canvas, Header, Sidebar } from "../components";
+import { Box } from "@mui/material";
 
 export function CanvasScreen() {
-  useEffect(() => {
-    drawCanvas();
-  }, []);
-
   return (
-    <Box className="CanvasScreen" sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex" }}>
       <Header />
       <Sidebar />
-      <Box
-        component="main"
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          flexGrow: 1,
-          height: "100vh",
-        }}
-      >
-        <Toolbar />
-        <TransformWrapper
-          initialScale={4}
-          centerOnInit={true}
-          minScale={4}
-          maxScale={10}
-        >
-          <TransformComponent
-            wrapperStyle={{
-              height: "100%",
-              width: "100%",
-            }}
-          >
-            <canvas id="canvas" width="200" height="200"></canvas>
-          </TransformComponent>
-        </TransformWrapper>
-        <ActiveEmoji />
-      </Box>
+      <Canvas />
     </Box>
   );
 }
-
-// pointer-events="none"
