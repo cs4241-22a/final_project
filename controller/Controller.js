8bb69b545556548cb7857191598a1a00f1ff0289
Glitch (pushbox) <none>
2022-10-11 02:32:52 +0000

    Initial Timer functionality

--- controller/Controller.js
+++ controller/Controller.js
@@ -1,16 +1,20 @@
 // import Model from '../model/Model.js';
 
 function move(model, dir) {
-    if (model.puzzle.canMove(dir)) {
-
-        model.puzzle.move(dir);
-        model.numMoves += 1;
-        model.isWin();
-        return model.copy();
+  if (model.puzzle.canMove(dir)) {
+    if (firstMove == true) {
+      startTimer();
+      firstMove = false;
     }
-    else return model;
+
+    model.puzzle.move(dir);
+    model.numMoves += 1;
+    model.isWin();
+    return model.copy();
+  } else return model;
 }
 
 function reset(model) {
-    return new Model(model.info);
+  firstMove = true;
+  return new Model(model.info);
 }
