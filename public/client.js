369990b872e5a8342be7e7e95a334236bd5cc7bf
sidneygoldinger <98048123+sidneygoldinger@users.noreply.github.com>
2022-10-12 17:39:17 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,89 +1,121 @@
-<!--create/submit playlists page -->
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Submit</title>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
+// client-side js
+// run by the browser each time your view template is loaded
 
-  <body class="bg-light">
-    <h1 class="text-primary ms-1">Submit a Song</h1>
-    <p>
-      Enter the song's genre ('Rap', 'Pop', 'R&B', 'Rock', or 'Country'), along
-      with the song's title and artist
-    </p>
-    <form>
-      <!--<input type="text" id="genre" value="enter a valid genre" />
-      <select id="genre" name="genre" size="3">
-        <option value="Rap">Rap</option>
-        <option value="saab">Saab</option>
-        <option value="fiat">Fiat</option>
-'Pop', 'R&B', 'Rock', or 'Country
-        <option value="audi">Audi</option>
-      </select>-->
-      <select class="form-select form-select-sm" aria-label=".form-select-sm example" id="genre">
-        <option selected>Genre</option>
-        <option value="Rap">Rap</option>
-        <option value="R&B">R&B</option>
-        <option value="Rock">Rock</option>
-        <option value="Pop">Pop</option>
-        <option value="Country">Country</option>
-      </select>
-      <br>
-      <input type="text" id="title" value="enter song title" />
-       <br>
-      <input type="text" id="artist" value="enter song artist" />
-       <br>
-      <button>Submit Song</button>
-    </form>
-    <form>
-      <button type="backToTop" id="backToTop">
-        Back to Top Songs Playlists
-      </button>
-    </form>
-    
-  </body>
-</html>
+//console.log("hello world :o");
 
-<script>
+// our default array of dreams
+
+// define variables that reference elements on our page
+
+const loginbtn = document.getElementById("loginbtn");
+const registerbtn = document.getElementById("registerbtn");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const usernameRInput = document.getElementById("newusername");
+const passwordRInput = document.getElementById("newpassword");
+const loginDiv = document.getElementById("login");
+const registerDiv = document.getElementById("register");
+const loginTogglebtn = document.getElementById("loginTogglebtn");
+const registerTogglebtn = document.getElementById("registerTogglebtn");
+let user, pass
+
+
+// a helper function that creates a list item for a given dream
+//loginPage.setAttribute("hidden", "hidden");
+        //plannerPage.removeAttribute("hidden");
+
+
+
+// iterate through every dream and add it to our page
+
+// listen for the form to be submitted and add a new dream when it is
+//Form.onsubmit = function (event) {
+
+//};
+
+
+
+const onClickLogin = function () {
+  user = usernameInput.value;
+  pass = passwordInput.value;
+
+  fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: user,
+      password: pass,
+    }),
+  })
+    .then((response) => response.json())
+    .then((login) => {
+      if (login) {
+        //loginPage.setAttribute("hidden", "hidden");
+        //plannerPage.removeAttribute("hidden");
+        //updateTable(user);
+        //warn.innerHTML = "";
+        window.location.href = 'top.html';
+        //TODO: set page (home page)
+        console.log("logged in");
+      } else {
+        
+        console.log("not logged in")
+        
+        //TODO: tell them wrong username or password
+        
+        
+        //warn.innerHTML = "incorrect username or password";
+      }
+    });
+
+  return false;
+};
+
+const onClickRegister = function () {
+  user = usernameRInput.value;
+  pass = passwordRInput.value;
   
-  const backBtn = document.getElementById('backToTop');
-  
-  const submit = function (e) {
-    e.preventDefault();
 
-    const genre = document.getElementById("genre"),
-      title = document.getElementById("title"),
-      artist = document.getElementById("artist")
-      //json = { genre: genre.value, title: title.value, artist: artist.value },
-      //body = JSON.stringify(json);
-      //console.log(body)
-    if(genre.value != 'Genre'){
-      fetch("/submit", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({genre: genre.value, title: title.value, artist: artist.value, vote: 1 }),
-      })
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(json) {
-        console.log(json);
-      });
+  fetch("/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: user,
+      password: pass,
+    }),
+  })
+    .then((response) => response.json())
+    .then((reg) => {
+      if (reg) {
+        console.log("registered")
+        
+        usernameInput.value = user;
+        passwordInput.value = pass;
+        onClickLogin();
+        //warn.innerHTML = "registered";
+      } else {
+        
+        console.log("not registered")
+        //warn.innerHTML = "username taken";
+      }
+    });
 
-      window.location.href = 'top.html';
-    }
-    return false;
-  };
-  
-  backBtn.onclick = () => {
-  window.location.href = 'top.html';
+  return false;
+};
+
+const loginVisible = function() {
+  loginDiv.removeAttribute("hidden");
+  registerDiv.setAttribute("hidden", "hidden");
   return false;
 }
 
-  window.onload = function () {
-    const button = document.querySelector("button");
-    button.onclick = submit;
-  };
-</script>
+const registerVisible = function() {
+  loginDiv.setAttribute("hidden", "hidden");
+  registerDiv.removeAttribute("hidden");
+  return false;
+}
+
+loginTogglebtn.onclick = loginVisible;
+registerTogglebtn.onclick = registerVisible;
+loginbtn.onclick = onClickLogin;
+registerbtn.onclick = onClickRegister;
