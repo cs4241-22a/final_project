c4b4c0ba25155e4cc86a9e012bbecf704e0612a5
HavanaHail <haileya4@nycischool.org>
2022-10-12 15:47:48 -0400

    svelte running stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
+});
+
+window.app = app;
+
+export default app;
