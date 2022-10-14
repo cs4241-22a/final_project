7360e4e6266edf1dfd57359dae353cc681cd3b90
Gabi Tessier <gabi246>
2022-10-13 20:41:02 -0400

    css

--- build/_snowpack/pkg/react.js
+++ build/_snowpack/pkg/react.js
@@ -3,6 +3,5 @@ export { r as default } from './common/index-aa16cd9b.js';
 
 
 
-var createRef = react.createRef;
 var useState = react.useState;
-export { createRef, useState };
+export { useState };
