5f7d48ed0ac8cf6dd996137380665c5374bca4cb
Gabe Camacho <gabestuf@gmail.com>
2023-01-12 02:16:33 -0500

    v

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const db = mongoose.connection
 db.on('error', (error) => { console.error(error) })
 db.once('open', () => {
     console.log("Connected to database");
-    app.listen(process.env.PORT || 3000, () => console.log(`Server Started on Port ${process.env.PORT || 3000}`));
+    app.listen(3000 || process.env.PORT, () => console.log(`Server Started on Port ${process.env.PORT || 3000}`));
 })
 
 // app.listen(3000, () => console.log('Server Started on Port 3000'));
